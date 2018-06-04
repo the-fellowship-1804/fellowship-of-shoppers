beee207813bc11c05f0015e6c692483564b76b32
Chris Ardeljan <cardeljan@gmail.com>
2018-06-04 16:41:49 -0400

    /server/api/products - added post route, made all routes async/await

--- server/api/products.js
+++ server/api/products.js
@@ -2,21 +2,34 @@ const router = require('express').Router();
 const { Product } = require('../db/models');
 module.exports = router;
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const products = Product.findAll();
+    const products = await Product.findAll();
     res.json(products);
   } catch (err) {
     next(err);
   }
 });
 
-router.get('/:productid', (req, res, next) => {
+router.get('/:productid', async (req, res, next) => {
   try {
     const productId = req.params.productid;
-    const product = Product.findById(productId);
+    const product = await Product.findById(productId);
     res.json(product);
   } catch (err) {
     next(err);
   }
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.setStatus(201).json(newProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// router put
+
+// router delete
