cbcb412a7ced6e116acaaeb6f8b4979d226fb03d
Chris Ardeljan <cardeljan@gmail.com>
2018-06-04 16:30:13 -0400

    /server/api/products - added get route for all products

--- server/api/products.js
+++ server/api/products.js
@@ -2,3 +2,11 @@ const router = require('express').Router();
 const { Product } = require('../db/models');
 module.exports = router;
 
+router.get('/', (req, res, next) => {
+  try {
+    const products = Product.findAll();
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
