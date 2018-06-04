c12a1a83794a7ec90573a3710363b1a3ddfff61d
dk268 <lorddanp@aol.com>
2018-06-04 16:04:34 -0400

    restored what was deleted

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,8 +1,8 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const crypto = require("crypto");
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const User = db.define('user', {
+const User = db.define("user", {
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -13,55 +13,67 @@ const User = db.define('user', {
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
-      return () => this.getDataValue('password')
+      return () => this.getDataValue("password");
     }
   },
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
-    get () {
-      return () => this.getDataValue('salt')
+    get() {
+      return () => this.getDataValue("salt");
     }
   },
   googleId: {
     type: Sequelize.STRING
-  }
-})
+  },
 
-module.exports = User
+  lastName: Sequelize.STRING,
+  firstName: Sequelize.STRING,
+  address: Sequelize.STRING,
+  //payment info
+  cart: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER)
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+});
+
+module.exports = User;
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function (candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
 
 /**
  * classMethods
  */
-User.generateSalt = function () {
-  return crypto.randomBytes(16).toString('base64')
-}
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString("base64");
+};
 
-User.encryptPassword = function (plainText, salt) {
+User.encryptPassword = function(plainText, salt) {
   return crypto
-    .createHash('RSA-SHA256')
+    .createHash("RSA-SHA256")
     .update(plainText)
     .update(salt)
-    .digest('hex')
-}
+    .digest("hex");
+};
 
 /**
  * hooks
  */
 const setSaltAndPassword = user => {
-  if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
+  if (user.changed("password")) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
   }
-}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
