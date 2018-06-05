b3be1be9db64e0973e8bf792d44c0f4f5bbc5985
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-05 16:55:43 -0400

    renames user

--- client/components/SingleUser.spec.js
+++ client/components/SingleUser.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {SingleUser} from './user-home'
+import {SingleUser} from './SingleUser'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
