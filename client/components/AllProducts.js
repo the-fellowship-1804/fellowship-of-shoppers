e3e6b4a6afb0ffabc24831d27142459331272c20
dk268 <lorddanp@aol.com>
2018-06-05 15:45:24 -0400

    productCard separatedfrom AllProducts; enjoy

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,63 +1,56 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux'
-import { getProducts } from '../store/allProducts'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getProducts } from "../store/allProducts";
 import { withRouter, Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
 
-const [UNASKED, LOADING,  LOADED, ERROR] = [
+const [UNASKED, LOADING, LOADED, ERROR] = [
   "UNASKED",
   "LOADING",
   "LOADED",
   "ERROR"
-]
+];
 
 class AllProducts extends Component {
-
   componentDidMount() {
-    this.props.getProducts()
+    this.props.getProducts();
   }
 
   render() {
     const allProducts = this.props.products;
-    switch(this.props.status) {
+    switch (this.props.status) {
       case UNASKED: {
-        return <p>we dont want this</p>
+        return <p>we dont want this</p>;
       }
       case LOADING: {
-        return <p>LOADING...</p>
+        return <p>LOADING...</p>;
       }
       case LOADED: {
         return (
           <div>
             <h1>All Products</h1>
-              {
-                allProducts.map((product) => {
-                  return (
-                    <div key={product.id}>
-                       {
-                      <Link to={`/products/${product.id}`}>
-                         <img src={product.productImageUrl} />
-                         <p>{product.productName}</p>
-                      </Link>
-                       }
-                       <p>{product.price ? product.price : 'Free'}</p>
-                    </div>
-                  )
-                })
-              }
+            {allProducts.map(product => {
+              return <ProductCard product={product} />;
+            })}
           </div>
-        )
+        );
       }
     }
   }
 }
 
-const mapSTP = (state) => {
+const mapSTP = state => {
   return {
     products: state.allProducts.products,
     status: state.allProducts.status
-  }
-}
+  };
+};
 
-const mapDTP = { getProducts }
+const mapDTP = { getProducts };
 
-export default withRouter(connect(mapSTP, mapDTP)(AllProducts));
+export default withRouter(
+  connect(
+    mapSTP,
+    mapDTP
+  )(AllProducts)
+);
