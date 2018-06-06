304479e035c6195a29f2f6459b8f71d305f9b9be
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-06 16:38:41 -0400

    updates put route to handle changing quantities

--- server/api/users.js
+++ server/api/users.js
@@ -8,7 +8,7 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email'],
+    attributes: ['id', 'email']
   })
     .then(users => res.json(users))
     .catch(next);
@@ -22,25 +22,46 @@ router.get('/:id', (req, res, next) => {
 });
 
 //Update a user's info
-router.put('/:id', (req, res, next) => {
-  if (req.body.addProductToCart) {
-    User.findById(req.params.id)
-      .then(user =>
-        user.update({
-          cart: [...user.cart, req.body.addProductToCart],
-        })
-      )
-      .then(res.end())
-      .catch(next);
-  } else {
-    console.log(req.body);
-    User.update(req.body, {
-      where: { id: req.params.id },
-      returning: true,
-      plain: true,
-    })
-      .then(updatedUser => res.json(updatedUser[1].dataValues))
-      .catch(next);
+router.put('/:id', async (req, res, next) => {
+  const addedProduct = req.body.productObj.product;
+  try {
+    if (req.body.addProductToCart) {
+      const user = await User.findById(req.params.id);
+      const matchedProduct = user.cart.filter(
+        productObj => productObj.product.id === addedProduct.id
+      );
+      if (matchedProduct.length) {
+        await user.update({
+          cart: [...user.cart, req.body.addProductToCart]
+        });
+      } else {
+        await user.update({
+          cart: [
+            ...user.cart.map(productObj => {
+              if (productObj.product.id === addedProduct.id) {
+                return {
+                  ...productObj,
+                  quantity: (productObj.quantity +=
+                    req.body.productObj.quantity)
+                };
+              } else {
+                return productObj;
+              }
+            })
+          ]
+        });
+      }
+      res.end();
+    } else {
+      const [, user] = await User.update(req.body, {
+        where: { id: req.params.id },
+        returning: true,
+        plain: true
+      });
+      res.json(user[0]);
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
@@ -48,8 +69,8 @@ router.put('/:id', (req, res, next) => {
 router.delete('/:id', (req, res, next) => {
   User.destroy({
     where: {
-      id: req.id,
-    },
+      id: req.id
+    }
   })
     .then(res.end())
     .catch(next);
