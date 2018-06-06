f17a8a9e5a1af00b1a256d776becaa2d6890b757
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-06 16:19:03 -0400

    updates thunk to store quantity of products added to cart

--- client/store/singleUser.js
+++ client/store/singleUser.js
@@ -55,9 +55,10 @@ export const logout = () => dispatch =>
     })
     .catch(err => console.log(err));
 
-export const addToCart = (userId, product) => async dispatch => {
+export const addToCart = (userId, product, quantity = 1) => async dispatch => {
+  const productObj = { product, quantity };
   try {
-    await axios.put(`/api/users/${userId}`, { addProductToCart: product });
+    await axios.put(`/api/users/${userId}`, { addProductToCart: productObj });
     dispatch(aCC(ADD_TO_CART, product));
   } catch (err) {
     console.log(err);
