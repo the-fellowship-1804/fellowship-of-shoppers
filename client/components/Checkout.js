109d0187757fb8cf8a1240b2a4b4a8022b4ecbb0
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-07 13:25:26 -0400

    adds skeleton of checkout component

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -8,10 +8,11 @@ class Cart extends React.Component {
   }
 
   handleClick = () => {
-    //redirect user to a checkout component
-    //where they can put in/edit their payment and shipping info and actually buy the item
-    //buying the item should unshift the cart into their order history and clear the cart in both DB model and store-state
+    //should unshift the cart into their order history and clear the cart in both DB model and store-state
+    //and redirect them to singleuser page
   };
+
+  //where they can put in/edit their payment and shipping info and actually buy the item
 
   caluculateTotalPrice = () => {
     let sum = 0;
@@ -24,7 +25,7 @@ class Cart extends React.Component {
   render() {
     return (
       <div>
-        <h3>Cart</h3>
+        <h3>Checkout</h3>
         <div>
           Your total is:{' '}
           {this.props.user.id
@@ -34,30 +35,35 @@ class Cart extends React.Component {
         <button type="button" onClick={this.handleClick}>
           Place Order
         </button>
-        {this.props.user.id
-          ? this.props.user.cart.map(productObj => (
-              <key={productObj.product.id}
-                productObj={productObj}
-              />
-            ))
-          : null}
+        {this.props.user.id ? (
+          <div>
+            <h3>Your items:</h3>
+            {this.props.user.cart.length > 0 ? (
+              this.props.user.cart.map(productObj => (
+                <ul key={productObj.product.id}>
+                  <h4>{productObj.product.name}</h4>
+                  <h4>{productObj.product.price}</h4>
+                  <h4>{productObj.quanitity}</h4>
+                  <h4>{productObj.product.price * productObj.quanitity}</h4>
+                </ul>
+              ))
+            ) : (
+              <div> no items </div>
+            )}
+          </div>
+        ) : null}
       </div>
     );
   }
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     user: state.singleUser
   };
 };
 
-const mapProps = { removeFromCart };
-
 export default connect(
   mapState,
-  mapProps
+  null
 )(Cart);
