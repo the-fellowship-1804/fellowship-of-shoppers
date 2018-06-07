6fd184678bc59f7882cc21aae937bf3d05a978bc
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-06 20:10:02 -0400

    splits productcard into checkoutcard and productcard components

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,38 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getProducts } from '../store/allProducts';
-import { withRouter, Link } from 'react-router-dom';
-
-const [UNASKED, LOADING, LOADED, ERROR] = [
-  'UNASKED',
-  'LOADING',
-  'LOADED',
-  'ERROR',
-];
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductCard = props => {
-  const product = props.productObj ? props.productObj.product : props.product;
   return (
     <div>
       {
-        <Link to={`/products/${product.id}`}>
-          <img src={product.productImageUrl} />
-          <p>{product.productName}</p>
+        <Link to={`/products/${props.product.id}`}>
+          <img src={props.product.productImageUrl} />
+          <p>{props.product.productName}</p>
         </Link>
       }
-      <p>Price: {product.price ? product.price : 'Free'}</p>
-      {props.onCheckoutPage ? (
-        <div>
-          <div>Quantity: {props.productObj.quantity} </div>
-          <button
-            type="button"
-            onClick={event => props.handleClickRemove(event, product.id)}
-            //passed in from Checkout
-          >
-            Remove from Cart
-          </button>
-        </div>
-      ) : null}
+      <p>Price: {props.product.price ? props.product.price : 'Free'}</p>
     </div>
   );
 };
