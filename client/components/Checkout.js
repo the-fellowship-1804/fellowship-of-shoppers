ca4612e6d3999ccfca1c7ce1f6b09b7e7d974968
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-06 23:16:10 -0400

    refactors total-price, adds comments  for checkout

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -9,14 +9,10 @@ class Checkout extends React.Component {
     this.state = {};
   }
 
-  handleCheckout = () => {
-    //some bs to actually check the person out:
-    //charge their card
-    //add their cart's contents to their ordered history
-    //clear their cart
-    //redirect them somewhere? => place to enter payment info/address with options to use default?
-    //check if they have a valid address and payment method when the rest works
-    //
+  handleClick = () => {
+    //redirect user to a checkout component
+    //where they can put in/edit their payment and shipping info and actually buy the item
+    //buying the item should unshift the cart into their order history and clear the cart in both DB model and store-state
   };
 
   caluculateTotalPrice = () => {
@@ -28,16 +24,16 @@ class Checkout extends React.Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        <h3>Checkout</h3>
+        <h3>Cart</h3>
         <div>
           Your total is:{' '}
-          {this.props.user.id ? this.caluculateTotalPrice() : 'Calculating'}{' '}
-          space-cash
+          {this.props.user.id
+            ? `${this.caluculateTotalPrice()} space-cash`
+            : 'Calculating...'}
         </div>
-        <button type="button" onClick={this.handleCheckout}>
+        <button type="button" onClick={this.handleClick}>
           Checkout!
         </button>
         {this.props.user.id
