d185e12a271a8ccc846c5134fa61ae3be65784ff
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-07 20:01:52 -0400

    adds stripe's API route

---
+++ server/api/charge.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+module.exports = router;
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST_KEY);
+
+const charge = req => {
+  const token = req.body.stripeToken;
+  return stripe.charges.create({
+    amount: Number(req.params.price),
+    currency: process.env.STRIPE_CCY,
+    source: token
+  });
+};
+
+router.post('/:price', async (req, res, next) => {
+  console.log('REQ.BODY\n\n', req.body);
+  try {
+    const charged = await charge(req);
+    console.log('charged\n\n', charged);
+    res.json(charged);
+  } catch (error) {
+    next(error);
+  }
+});
