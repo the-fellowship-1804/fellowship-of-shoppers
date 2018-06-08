3bad509881de666d32b3479154c0d16175661276
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-08 13:18:32 -0400

    changes stripe script to load async

--- client/app.js
+++ client/app.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import Routes from './routes';
 import { Navbar } from './components';
+import { StripeProvider } from 'react-stripe-elements';
 
-const App = () => {
-  return (
-    <div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { stripe: null };
+  }
+  componentDidMount() {
+    if (window.Stripe) {
+      this.setState({
+        stripe: window.Stripe('pk_test_1BGAQuuZplpLNN1Y5QC5V08o')
+      });
+    } else {
+      document.querySelector('#stripe-js').addEventListener('load', () => {
+        // Create Stripe instance once Stripe.js loads
+        this.setState({
+          stripe: window.Stripe('pk_test_1BGAQuuZplpLNN1Y5QC5V08o')
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
       <div>
         <Navbar />
-        <Routes />
+        <StripeProvider stripe={this.state.stripe}>
+          <Routes />
+        </StripeProvider>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
