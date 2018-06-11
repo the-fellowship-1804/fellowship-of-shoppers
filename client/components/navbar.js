f954c71398297ffa264a5cf44562bf3e9ff8e780
Chris Ardeljan <cardeljan@gmail.com>
2018-06-11 13:10:31 -0400

    Feat:CSS - Made sure only navbar links were white, other links from Product Cards now display

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,11 +9,11 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
     <nav>
       {isLoggedIn ? (
         <div className="rowcontainer">
-          <div>
+          <div className="navlinks">
             <Link to="/">Home</Link>
             <Link to="/products">All Products</Link>
           </div>
-          <div>
+          <div className="navlinks">
             <Link to="/user">Your Account</Link>
             <a href="#" onClick={handleClick}>
               Logout
@@ -22,19 +22,19 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
           </div>
         </div>
       ) : (
-          <div className="rowcontainer">
-            <div>
-              <Link to="/">Home</Link>
-              <Link to="/products">All Products</Link>
-            </div>
-            {/* The navbar will show these links before you log in */}
-            <div>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-              <Link to="/cart">Cart</Link>
-            </div>
+        <div className="rowcontainer">
+          <div className="navlinks">
+            <Link to="/">Home</Link>
+            <Link to="/products">All Products</Link>
           </div>
-        )}
+          {/* The navbar will show these links before you log in */}
+          <div className="navlinks">
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/cart">Cart</Link>
+          </div>
+        </div>
+      )}
     </nav>
   </div>
 );
