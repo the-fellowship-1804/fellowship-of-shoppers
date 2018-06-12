378dee9702f27e6b384ca7814f6970bf00b30170
dk268 <lorddanp@aol.com>
2018-06-12 18:20:38 -0400

    Feat. add: adds more of DK's tests

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -3,6 +3,66 @@ const request = require('supertest');
 const db = require('../db');
 const app = require('../index');
 const Product = db.model('product');
+import supertest from 'supertest';
+import chai from 'chai';
+import chaiProperties from 'chai-properties';
+import chaiThings from 'chai-things';
+chai.use(chaiProperties);
+chai.use(chaiThings);
+
+describe('Product routes', () => {
+  beforeEach('Sync and clear database', () => db.sync({ force: true }));
+
+  after('Synchronize and clear database', () => db.sync({ force: true }));
+
+  describe('Server', () => {
+    let agent;
+    beforeEach('Set up agent for testing', () => {
+      agent = supertest(app);
+    });
+
+    describe('api routes', () => {
+      let deathStar;
+      let xwing;
+      beforeEach('Seed products', () => {
+        const products = [
+          { name: 'X-Wing', firepower: 120 },
+          { name: 'The Death Star', firepower: 50000 }
+        ];
+        return Product.bulkCreate(products, { returning: true }).then(
+          createdProducts => {
+            xwing = createdProducts[0].id;
+            deathStar = createdProducts[1].id;
+          }
+        );
+      });
+
+      describe('products', () => {
+        it('serves up all products on request to GET /', () => {
+          return agent
+            .get(`/api/products`)
+            .expect(200)
+            .then(res => {
+              expect(res.body).to.be.an('array');
+              expect(res.body.length).to.be.equal(2);
+              expect(res.body).to.contain.a.thing.with('id', xwing);
+              expect(res.body).to.contain.a.thing.with('id', deathStar);
+            });
+        });
+
+        it('serves a single product on a GET to /:productid', () => {
+          return agent
+            .get(`/api/products/${xwing}`)
+            .expect(200)
+            .then(res => {
+              expect(res.body).to.be.an('object');
+              expect(res.body).to.include({ name: 'X-Wing', firepower: 120 });
+            });
+        });
+      });
+    });
+  });
+});
 
 describe('Product routes', async () => {
   beforeEach(() => {
