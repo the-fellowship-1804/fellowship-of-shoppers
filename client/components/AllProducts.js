84935964080fd42f5930c2381154e3385d83b687
Chris Ardeljan <cardeljan@gmail.com>
2018-06-11 22:29:19 -0400

    Added and integrated filter and sort functionality to all products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -4,6 +4,7 @@ import { getProducts } from '../store/allProducts';
 import { withRouter, Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
 import Filters from './AllProductsFilters';
+import Sort from './AllProductsSort';
 
 const [UNASKED, LOADING, LOADED, ERROR] = [
   'UNASKED',
@@ -13,33 +14,83 @@ const [UNASKED, LOADING, LOADED, ERROR] = [
 ];
 
 class AllProducts extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       filter: false,
-      class: 'All'
+      class: 'All',
+      sort: 'byname'
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.applySort = this.applySort.bind(this);
   }
 
   componentDidMount() {
     this.props.getProducts();
   }
 
-  handleChange(event) {
-    console.dir(event.target);
+  applyFilter(event) {
+    // console.dir(event.target);
     this.setState({
+      ...this.state,
       filter: true,
       class: event.target.value
     });
+    if (event.target.value === 'All') this.setState({ filter: false });
+  }
+
+  applySort(event) {
+    // console.dir(event.target);
+    this.setState({ sort: event.target.value });
   }
 
   render() {
-    const allProducts = this.state.filter
+    console.log(this.state);
+    let allProducts = this.state.filter
       ? this.props.products.filter(
           ship => ship.class === this.state.class || this.state.class === `All`
         )
       : this.props.products;
+
+    if (this.state.sort === 'byname') {
+      allProducts = allProducts.sort((ObjA, ObjB) => {
+        const nameA = ObjA.name,
+          nameB = ObjB.name;
+        if (nameA < nameB) return -1;
+        if (nameB < nameA) return 1;
+        return 0;
+      });
+    }
+
+    if (this.state.sort === 'byfirepower') {
+      allProducts = allProducts.sort((ObjA, ObjB) => {
+        const fireA = ObjA.firepower,
+          fireB = ObjB.firepower;
+        if (fireA < fireB) return 1;
+        if (fireB < fireA) return -1;
+        return 0;
+      });
+    }
+
+    if (this.state.sort === 'bypricehightolow') {
+      allProducts = allProducts.sort((ObjA, ObjB) => {
+        const priceA = ObjA.price,
+          priceB = ObjB.price;
+        if (priceA < priceB) return 1;
+        if (priceB < priceA) return -1;
+        return 0;
+      });
+    }
+
+    if (this.state.sort === 'bypricelowtohigh') {
+      allProducts = allProducts.sort((ObjA, ObjB) => {
+        const priceA = ObjA.price,
+          priceB = ObjB.price;
+        if (priceA < priceB) return -1;
+        if (priceB < priceA) return 1;
+        return 0;
+      });
+    }
 
     switch (this.props.status) {
       case UNASKED: {
@@ -55,10 +106,13 @@ class AllProducts extends Component {
               All Products{' '}
               {this.state.class === 'All' ? `` : `> ${this.state.class}s`}
             </h2>
-            <Filters
-              handleChange={this.handleChange}
-              class={this.state.class}
-            />
+            <div>
+              <Filters
+                applyFilter={this.applyFilter}
+                class={this.state.class}
+              />
+              <Sort applySort={this.applySort} sort={this.state.sort} />
+            </div>
             {allProducts.map(product => {
               return <ProductCard key={product.id} product={product} />;
             })}
--- client/components/AllProductsFilters.js
+++ client/components/AllProductsFilters.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Filters = props => {
   return (
-    <select id="filter" name="filter" onChange={props.handleChange}>
+    <select id="filter" name="filter" onChange={props.applyFilter}>
       <option value="All">
         {props.class === 'All' ? 'Filter by Class' : 'All'}
       </option>
