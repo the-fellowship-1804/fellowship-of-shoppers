8f27a069f59ef3b06faff1c82301e16c3c36a3d2
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-12 14:12:22 -0400

    adds get /user and get /used/:id tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,5 +1,3 @@
-/* global describe beforeEach it */
-
 const { expect } = require('chai');
 const request = require('supertest');
 const db = require('../db');
@@ -7,28 +5,37 @@ const app = require('../index');
 const User = db.model('user');
 
 describe('User routes', () => {
-  beforeEach(() => {
-    return db.sync({ force: true });
+  let TU;
+  before(async () => {
+    try {
+      await db.sync({ force: true });
+      TU = await User.create({
+        email: 'T@E.com',
+        password: 'pw'
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    describe('/api/users/', () => {
+      it('GET /api/users gets all users as an array', () => {
+        return request(app)
+          .get('/api/users')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array');
+            expect(res.body[0].email).to.be.equal('T@E.com');
+          });
+      });
+      it('GET /api/users/:id gets one user as an object', () => {
+        return request(app)
+          .get('/api/users/1')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('object');
+            expect(res.body[0].email).to.be.equal('T@E.com');
+          });
+      });
+    });
   });
-
-  //   describe('/api/users/', () => {
-  //     const codysEmail = 'cody@puppybook.com';
-
-  //     beforeEach(() => {
-  //       return User.create({
-  //         email: codysEmail,
-  //         password: 'bones',
-  //       });
-  //     });
-
-  //     it('GET /api/users', () => {
-  //       return request(app)
-  //         .get('/api/users')
-  //         .expect(200)
-  //         .then(res => {
-  //           expect(res.body).to.be.an('array');
-  //           expect(res.body[0].email).to.be.equal(codysEmail);
-  //         });
-  //     });
-  //   }); // end describe('/api/users')
-}); // end describe('User routes')
+});
