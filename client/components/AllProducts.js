a38d1693ed33618b414e911be20ab42c01b05081
Chris Ardeljan <cardeljan@gmail.com>
2018-06-13 01:44:38 -0400

    Finalized admin functionality - except for update items but it's late

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -53,7 +53,7 @@ class AllProducts extends Component {
     if (this.state.sort === 'byname') {
       allProducts = allProducts.sort((ObjA, ObjB) => {
         const nameA = ObjA.name.toLowerCase(),
-          nameB = ObjB.toLowerCase();
+          nameB = ObjB.name.toLowerCase();
         if (nameA < nameB) return -1;
         if (nameB < nameA) return 1;
         return 0;
